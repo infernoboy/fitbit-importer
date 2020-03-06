b5b37ee163866ec7d0cce99700e9113ef827fe3f
Travis Roman <ffx1033@gmail.com>
2020-03-06 15:08:45 -0500

    Get shortcut name from fitbit’s state param

--- authorize.js
+++ authorize.js
@@ -1,8 +1,6 @@
 /*
-* @Last modified in Sublime on Mar 06, 2020 01:19:40 PM
+* @Last modified in Sublime on Mar 06, 2020 03:07:50 PM
 */
-
-const shortcutHelperName = 'Fitbit Importer Helper';
 
 function getHashParam(name) {
   var match = RegExp(`[#&]${name}=([^&]*)`).exec(window.location.hash);
@@ -12,6 +10,7 @@ function getHashParam(name) {
 document.addEventListener('DOMContentLoaded', function () {
 	const token = getHashParam('access_token');
 	const userID = getHashParam('user_id');
+	const shortcutName = getHashParam('state');
 
 	const link = document.querySelector('#open-fitbit-importer');
 
@@ -21,6 +20,6 @@ document.addEventListener('DOMContentLoaded', function () {
 		link.addEventListener('click', function (event) {
 			event.preventDefault();
 
-			window.location.href = `shortcuts://run-shortcut?name=${encodeURIComponent(shortcutHelperName)}&input=text&text={"command":"save-authorization","authorization":{"access_token":"${token}","user_id":"${userID}"}}`;
+			window.location.href = `shortcuts://run-shortcut?name=${encodeURIComponent(shortcutName)}&input=text&text={"command":"save-authorization","authorization":{"access_token":"${token}","user_id":"${userID}"}}`;
 		}, true);
 }, true);
